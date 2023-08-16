0052483d117545c5cf85460fa669a7641c0d641a
Somdipto Chakraborty <108862434+trap-representation@users.noreply.github.com>
2023-08-16 23:32:47 +0530

    `nvm_paths.h` isn't needed anymore

--- toc/src/nvm_paths.h
+++
@@ -1,7 +0,0 @@
-#ifndef TOC_NVM_PATHS_H
-#define TOC_NVM_PATHS_H
-
-#define TOC_NVM_IMPLEMENTATION_H "../../../nightVM/src/implementation.h"
-#define TOC_NVM_OPCODES_H "../../../nightVM/src/opcodes.h"
-
-#endif
